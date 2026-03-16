f160adab08d29bbabb3049c6615f23219d036c2e
korikashi <pedarkasy@gmail.com>
2026-03-16 10:44:35 -0500

    Backend listo, falta base de datos

--- conexion.php
+++ conexion.php
@@ -1,1 +1,14 @@
-// aqui va la conexion a la base de datos
+<?php
+
+$host = "localhost"; // cambiar cuando se suba a InfinityFree
+$user = "root";
+$password = "";
+$db = "contacto_lasalle";
+
+$conn = new mysqli($host, $user, $password, $db);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+
+?>
--- enviar.php
+++ enviar.php
@@ -1,1 +1,20 @@
-// aqui se guarda la informacion recibida de contacto.html en la base de datos
+<?php
+
+include "conexion.php";
+
+$nombre = $_POST["nombre"];
+$correo = $_POST["correo"];
+$mensaje = $_POST["mensaje"];
+
+$sql = "INSERT INTO mensajes (nombre, correo, mensaje)
+VALUES ('$nombre','$correo','$mensaje')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Mensaje enviado correctamente";
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$conn->close();
+
+?>
