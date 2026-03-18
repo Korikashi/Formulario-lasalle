61e16ca845bbfe33e55969f176195c9a2075588c
korikashi <pedarkasy@gmail.com>
2026-03-18 17:25:30 -0500

    Correcciones

--- mensajes.php
+++ mensajes.php
@@ -14,7 +14,10 @@ $result = $conn->query($sql);
 <tr>
 <th>ID</th>
 <th>Nombre</th>
+<th>Apellido</th>
 <th>Correo</th>
+<th>Telefono</th>
+<th>Programa</th>
 <th>Mensaje</th>
 <th>Fecha</th>
 </tr>
@@ -25,7 +28,10 @@ while($row = $result->fetch_assoc()){
     echo "<tr>";
     echo "<td>".$row["id"]."</td>";
     echo "<td>".$row["nombre"]."</td>";
+    echo "<td>".$row["apellido"]."</td>";
     echo "<td>".$row["email"]."</td>";
+    echo "<td>".$row["telefono"]."</td>";
+    echo "<td>".$row["programa"]."</td>";
     echo "<td>".$row["mensaje"]."</td>";
     echo "<td>".$row["fecha"]."</td>";
     echo "</tr>";
