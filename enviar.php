d91359791f3fb96fe45425fb0df827267460916c
korikashi <pedarkasy@gmail.com>
2026-03-19 20:37:38 -0500

    Arreglos para guardar informacion completa en la base de datos

--- enviar.php
+++ enviar.php
@@ -10,12 +10,19 @@ if(empty($_POST["nombre"]) || empty($_POST["email"]) || empty($_POST["mensaje"])
     die("Todos los campos son obligatorios");
 }
 
+
+
 $nombre = trim($_POST["nombre"]);
+$apellido = trim($_POST["apellido"]);
 $email = trim($_POST["email"]);
+$telefono = trim($_POST["telefono"]);
+$programa = trim($_POST["programa"]);
+$asunto = trim($_POST["asunto"]);
 $mensaje = trim($_POST["mensaje"]);
 
-$sql = "INSERT INTO mensajes (nombre, email, mensaje)
-VALUES ('$nombre','$email','$mensaje')";
+
+$sql = "INSERT INTO mensajes (nombre, apellido, email, telefono, programa, asunto, mensaje)
+VALUES ('$nombre', '$apellido', '$email', '$telefono', '$programa', '$asunto', '$mensaje')";
 
 if ($conn->query($sql) === TRUE) {
 
